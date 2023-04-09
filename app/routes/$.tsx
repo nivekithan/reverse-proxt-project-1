14f1bd7585b9763c8993334a88f9055ffe198dbc
Nivekithan S <nivekithan363@gmail.com>
2023-04-09 20:25:24 +0530

    Debug headers

--- app/routes/$.tsx
+++ app/routes/$.tsx
@@ -3,7 +3,9 @@ import { V2_MetaFunction, useLoaderData } from "@remix-run/react";
 
 export const loader = ({ request }: LoaderArgs) => {
   const orignalUrl = request.headers.get("x-original-url");
-  return json({ requestUrl: request.url, orignalUrl });
+
+  const headers = Array.from(request.headers.keys());
+  return json({ requestUrl: request.url, orignalUrl, headers });
 };
 
 export const meta: V2_MetaFunction = () => {
@@ -14,7 +16,7 @@ export default function Index() {
   const { requestUrl, orignalUrl } = useLoaderData<typeof loader>();
   return (
     <div>
-      <h1>Remix Website: 1</h1>
+      <h1>Remix Website: 2</h1>
       <p>Request URL: {requestUrl}</p>
       <p>Orignal URL: {orignalUrl}</p>
     </div>
